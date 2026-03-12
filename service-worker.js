e47b1f9850e2a3884759d39d50c00d2a2a439939
pusername1 <prem.n@seznam.cz>
2026-03-12 19:26:55 +0100

    Add files via upload
    v1

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = 'markets-cache-v1';
+const SHELL = [
+  './',
+  './index.html',
+  'https://unpkg.com/lightweight-charts/dist/lightweight-charts.standalone.production.js'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  const isJSON = url.pathname.startsWith('/data/');
+  if (isJSON) {
+    e.respondWith(fetch(e.request).then(r=>{
+      const copy = r.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return r;
+    }).catch(()=>caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+  }
+});
